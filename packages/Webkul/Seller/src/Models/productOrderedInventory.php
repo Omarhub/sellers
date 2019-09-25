5e320404ee07a9c7b4e75a7878e702b58f166aa1
Omar <oa8126@me.com>
2019-09-25 23:21:52 +0300

    refactoringg

--- packages/Webkul/Seller/src/Models/productOrderedInventory.php
+++ packages/Webkul/Seller/src/Models/productOrderedInventory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 use Webkul\Product\Models\ProductOrderedInventory as ProductOrderedInventoryBaseModel;
 
-class productOrderedInventory extends ProductOrderedInventoryBaseModel
+class ProductOrderedInventory extends ProductOrderedInventoryBaseModel
 {
     protected $fillable = ['qty', 'product_id', 'channel_id', 'vendor_id'];
 }
